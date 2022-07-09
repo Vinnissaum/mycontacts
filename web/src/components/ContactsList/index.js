3ab35fe87a5b4086fb1f224c04fa3b99495ef865
Vinicius Braz <vinicius_lsb@live.com>
2022-07-09 10:56:34 -0300

    feat: implements card component

--- web/src/components/ContactsList/index.js
+++ web/src/components/ContactsList/index.js
@@ -1,5 +1,9 @@
-import { Container, Header, ListContainer } from './styles';
+import {
+  Container, Header, ListContainer, Card,
+} from './styles';
 import arrow from '../../assets/images/icons/arrow.svg';
+import edit from '../../assets/images/icons/edit.svg';
+import trash from '../../assets/images/icons/trash.svg';
 
 export default function ContactsList() {
   return (
@@ -15,6 +19,60 @@ export default function ContactsList() {
             <img src={arrow} alt="Arrow" />
           </button>
         </header>
+        <Card>
+          <div className="info">
+            <div className="contact-name">
+              <strong>Vinicius Braz</strong>
+              <small>instagram</small>
+            </div>
+            <span>vinicius_lsb@live.com</span>
+            <span>(12) 99999-9999</span>
+          </div>
+          <div className="actions">
+            <a href="/">
+              <img src={edit} alt="Edit" />
+            </a>
+            <button type="button">
+              <img src={trash} alt="Delete" />
+            </button>
+          </div>
+        </Card>
+        <Card>
+          <div className="info">
+            <div className="contact-name">
+              <strong>Vinicius Braz</strong>
+              <small>instagram</small>
+            </div>
+            <span>vinicius_lsb@live.com</span>
+            <span>(12) 99999-9999</span>
+          </div>
+          <div className="actions">
+            <a href="/">
+              <img src={edit} alt="Edit" />
+            </a>
+            <button type="button">
+              <img src={trash} alt="Delete" />
+            </button>
+          </div>
+        </Card>
+        <Card>
+          <div className="info">
+            <div className="contact-name">
+              <strong>Vinicius Braz</strong>
+              <small>instagram</small>
+            </div>
+            <span>vinicius_lsb@live.com</span>
+            <span>(12) 99999-9999</span>
+          </div>
+          <div className="actions">
+            <a href="/">
+              <img src={edit} alt="Edit" />
+            </a>
+            <button type="button">
+              <img src={trash} alt="Delete" />
+            </button>
+          </div>
+        </Card>
       </ListContainer>
     </Container>
   );
