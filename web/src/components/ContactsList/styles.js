e3f3e90096142a05a16641f4b183f2e702c62a7c
Vinicius Braz <vinicius_lsb@live.com>
2022-07-09 09:56:16 -0300

    style: implements the ContactsList's Header

---
+++ web/src/components/ContactsList/styles.js
@@ -0,0 +1,32 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  margin-top: 32px;
+`;
+
+export const Header = styled.header`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  strong {
+    color: #222;
+    font-size: 24px;
+  }
+
+  a {
+    padding: 8px 16px;
+
+    color: ${({ theme }) => theme.colors.primary.main};
+    text-decoration: none;
+    font-weight: bold;
+    border: 2px solid #5061FC;
+    border-radius: 4px;
+    transition: all 0.2s ease-in;
+
+    &:hover {
+      background: ${({ theme }) => theme.colors.primary.main};
+      color: #FFF;
+    }
+  }
+`;
