63f1f139e6dcf55d8cf837c26960d9960b2ab6c9
Vinicius Braz <vinicius_lsb@live.com>
2022-07-09 11:31:12 -0300

    feat: set main routes

--- web/src/components/App/index.js
+++ web/src/components/App/index.js
@@ -1,20 +1,23 @@
 import { ThemeProvider } from 'styled-components';
+import { BrowserRouter } from 'react-router-dom';
 
 import GlobalStyle from '../../assets/styles/global';
 import defaultTheme from '../../assets/styles/themes/default';
 import Container from './styles';
 import Header from '../Header';
-import ContactsList from '../ContactsList';
+import Routes from '../../routes';
 
 function App() {
   return (
-    <ThemeProvider theme={defaultTheme}>
-      <GlobalStyle />
-      <Container>
-        <Header />
-        <ContactsList />
-      </Container>
-    </ThemeProvider>
+    <BrowserRouter>
+      <ThemeProvider theme={defaultTheme}>
+        <GlobalStyle />
+        <Container>
+          <Header />
+          <Routes />
+        </Container>
+      </ThemeProvider>
+    </BrowserRouter>
   );
 }
 
