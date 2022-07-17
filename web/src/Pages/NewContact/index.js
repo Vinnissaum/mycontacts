95fde6eeb082af0e102ca20e22343fca9e882fa9
Vinicius Braz <vinicius_lsb@live.com>
2022-07-16 23:27:03 -0300

    feat: add Input and Button component

--- web/src/Pages/NewContact/index.js
+++ web/src/Pages/NewContact/index.js
@@ -1,7 +1,18 @@
+import Input from '../../components/Input';
+import Select from '../../components/Select';
 import PageHeader from '../../components/PageHeader';
+import Button from '../../components/Button';
 
 export default function NewContact() {
   return (
-    <PageHeader title="Novo contato" />
+    <>
+      <PageHeader title="Novo contato" />
+      <Input type="text" placeholder="Nome" />
+      <Select>
+        <option value="">Instagram</option>
+      </Select>
+      <Button>Salvar alterações</Button>
+      <Button disabled>Salvar alterações</Button>
+    </>
   );
 }
