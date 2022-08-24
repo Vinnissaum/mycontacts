d68c6bdf216d7ead9bcfa1d6df19015ef3e56ad2
Vinicius Braz <vinicius_lsb@live.com>
2022-08-24 18:29:33 -0300

    feat: renders api data

--- web/src/Pages/Home/index.js
+++ web/src/Pages/Home/index.js
@@ -1,5 +1,6 @@
 import { Link } from 'react-router-dom';
 
+import { useEffect, useState } from 'react';
 import {
   Container, InputSearchContainer, Header, ListContainer, Card,
 } from './styles';
@@ -9,13 +10,26 @@ import edit from '../../assets/images/icons/edit.svg';
 import trash from '../../assets/images/icons/trash.svg';
 
 export default function Home() {
+  const [contacts, setContacts] = useState([]);
+  useEffect(() => {
+    fetch('http://localhost:3001/contacts')
+      .then(async (response) => {
+        const json = await response.json();
+        setContacts(json);
+      });
+  }, []);
+
+  console.log(contacts);
   return (
     <Container>
       <InputSearchContainer>
         <input type="text" placeholder="Pesquise pelo nome..." />
       </InputSearchContainer>
       <Header>
-        <strong>3 contatos</strong>
+        <strong>
+          {contacts.length}
+          {contacts.length === 1 ? ' contato' : ' contatos' }
+        </strong>
         <Link to="/new">Novo Contato</Link>
       </Header>
       <ListContainer>
@@ -25,24 +39,26 @@ export default function Home() {
             <img src={arrow} alt="Arrow" />
           </button>
         </header>
-        <Card>
-          <div className="info">
-            <div className="contact-name">
-              <strong>Vinicius Braz</strong>
-              <small>instagram</small>
+        {contacts.map((contact) => (
+          <Card key={contact.id}>
+            <div className="info">
+              <div className="contact-name">
+                <strong>{contact.name}</strong>
+                {contact.category_name && <small>{contact.category_name}</small>}
+              </div>
+              <span>{contact.email}</span>
+              <span>{contact.phone}</span>
             </div>
-            <span>vinicius_lsb@live.com</span>
-            <span>(12) 99999-9999</span>
-          </div>
-          <div className="actions">
-            <Link to="/edit/123">
-              <img src={edit} alt="Edit" />
-            </Link>
-            <button type="button">
-              <img src={trash} alt="Delete" />
-            </button>
-          </div>
-        </Card>
+            <div className="actions">
+              <Link to={`/edit/${contact.id}`}>
+                <img src={edit} alt="Edit" />
+              </Link>
+              <button type="button">
+                <img src={trash} alt="Delete" />
+              </button>
+            </div>
+          </Card>
+        ))}
       </ListContainer>
     </Container>
   );
