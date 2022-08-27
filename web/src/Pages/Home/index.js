9cfebcd1b9646f6268014afef8c5d05463c02505
Vinicius Braz <vinicius_lsb@live.com>
2022-08-27 18:02:52 -0300

    feat: implements loader and an util delay functions

--- web/src/Pages/Home/index.js
+++ web/src/Pages/Home/index.js
@@ -4,6 +4,8 @@ import { useEffect, useMemo, useState } from 'react';
 import {
   Container, InputSearchContainer, Header, Card, ListHeader,
 } from './styles';
+import Loader from '../../components/Loader';
+import delay from '../../utils/delay';
 
 import arrow from '../../assets/images/icons/arrow.svg';
 import edit from '../../assets/images/icons/edit.svg';
@@ -13,15 +15,19 @@ export default function Home() {
   const [contacts, setContacts] = useState([]);
   const [orderBy, setOrderBy] = useState('ASC');
   const [searchTerm, setSearchTerm] = useState('');
+  const [isContactsLoading, setIsContactsLoading] = useState(true);
   const filteredContacts = useMemo(() => contacts.filter((contact) => contact.name.toLowerCase()
     .includes(searchTerm.toLowerCase())), [contacts, searchTerm]);
 
   useEffect(() => {
     fetch(`http://localhost:3001/contacts?order=${orderBy}`)
       .then(async (response) => {
+        setIsContactsLoading(true);
+        await delay(2000);
         const json = await response.json();
         setContacts(json);
-      });
+      })
+      .finally(() => setIsContactsLoading(false));
   }, [orderBy]);
 
   const handleToggleOrderBy = () => {
@@ -34,6 +40,7 @@ export default function Home() {
 
   return (
     <Container>
+      <Loader isLoading={isContactsLoading} />
       <InputSearchContainer>
         <input
           type="text"
